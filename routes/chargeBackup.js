7a06484acd2f38f4647c4ed79024077b3f56a819
33123312 <65249051+33123312@users.noreply.github.com>
2021-11-23 18:41:13 -0700

    hice que cuanddod se hagan respaldos el servidor retprne el respaldo recién creado

--- routes/chargeBackup.js
+++ routes/chargeBackup.js
@@ -47,7 +47,7 @@ function concactAndExecute(route,file){
 }
 
 function executeShell(comand){
-    let shell = "mysql -u " + user+ " -p" + password + "  backDatabase < " + comand
+    let shell = "mysql -u root -pPupet6113ñ$ backDatabase < " + comand
     shellExecuter(shell,()=>{
         response.sendStatus(200);
     })
